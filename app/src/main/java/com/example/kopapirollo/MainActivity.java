ea2f870c573bef15c85be24bcbd91ef52cbb2b75
Kiss Dániel <oldtrue69@gmail.com>
2020-10-18 18:35:07 +0200

    élet levonás,ki nyert,

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -6,61 +6,158 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.IllegalFormatCodePointException;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView gepHp1,gepHp2,gepHp3,gepValasz,sajatHp1,sajatHp2,sajatHp3,sajatValasz;
+    private ImageView gepHp1,gepHp2,gepHp3,imageGepValasz,sajatHp1,sajatHp2,sajatHp3,imageSajatValasz;
     private Button btnRock,btnPaper,btnScissor;
     private TextView dontetlenek;
-    private int[] images={R.drawable.rock,R.drawable.paper,R.drawable.scissors};
+    private int sajatElet,gepElet,sajatValasz,gepValasz,dontetlenekSzama;
     private Random rand=new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        gombok();
-
+        Gombok();
 
 
     }
 
-    private void gombok() {
+    private void Gombok() {
         btnRock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gepValasz.setImageResource(images[rand.nextInt(images.length)]);
-                sajatValasz.setImageResource(R.drawable.rock);
+                gepValasz = (int)(Math.random()*4)+1;
+                sajatValasz=1;
+                imageSajatValasz.setImageResource(R.drawable.rock);
+                GepValaszVizsgal();
+                KiNyert();
             }
         });
         btnPaper.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gepValasz.setImageResource(images[rand.nextInt(images.length)]);
-                sajatValasz.setImageResource(R.drawable.paper);
+                gepValasz = (int)(Math.random()*4)+1;
+                sajatValasz=2;
+                imageSajatValasz.setImageResource(R.drawable.paper);
+                GepValaszVizsgal();
+                KiNyert();
             }
         });
         btnScissor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gepValasz.setImageResource(images[rand.nextInt(images.length)]);
-                sajatValasz.setImageResource(R.drawable.scissors);
+                gepValasz = (int)(Math.random()*4)+1;
+                sajatValasz=3;
+                imageSajatValasz.setImageResource(R.drawable.scissors);
+                GepValaszVizsgal();
+                KiNyert();
             }
         });
+        GepValaszVizsgal();
+        KiNyert();
+    }
 
+
+    private void KiNyert(){
+        if (gepValasz == sajatValasz){
+            Toast.makeText(MainActivity.this,"Döntetlen",Toast.LENGTH_SHORT).show();
+            dontetlenekSzama+=1;
+            dontetlenek.setText("Döntetlenek száma: "+dontetlenekSzama);
+        }
+        if(gepValasz==1 && sajatValasz==2){
+            Toast.makeText(MainActivity.this,"Nyertél",Toast.LENGTH_SHORT).show();
+            GepEletLevon();
+        }
+        if (gepValasz==1 && sajatValasz==3) {
+            Toast.makeText(MainActivity.this,"A gép nyert",Toast.LENGTH_SHORT).show();
+            SajatEletLevon();
+        }if (gepValasz==2 && sajatValasz==1){
+            Toast.makeText(MainActivity.this,"A gép nyert",Toast.LENGTH_SHORT).show();
+            SajatEletLevon();
+        }if (gepValasz==2 && sajatValasz==3) {
+            Toast.makeText(MainActivity.this,"Te nyertél",Toast.LENGTH_SHORT).show();
+            GepEletLevon();
+        }if (gepValasz==3 && sajatValasz==1) {
+            Toast.makeText(MainActivity.this,"Te nyertél",Toast.LENGTH_SHORT).show();
+            GepEletLevon();
+        }if (gepValasz==3 && sajatValasz==2) {
+            Toast.makeText(MainActivity.this,"A gép nyert",Toast.LENGTH_SHORT).show();
+            SajatEletLevon();
+        }
+    }
+
+    private void GepValaszVizsgal() {
+        if (gepValasz==1){
+            imageGepValasz.setImageResource(R.drawable.rock);
+        }else if (gepValasz==2) {
+            imageGepValasz.setImageResource(R.drawable.paper);
+        }else if(gepValasz==3){
+            imageGepValasz.setImageResource(R.drawable.scissors);
+        }
+    }
+
+    private void SajatEletLevon() {
+
+        switch (sajatElet){
+
+            case 3:
+                sajatHp3.setImageResource(R.drawable.heart1);
+                break;
+            case 2:
+                sajatHp2.setImageResource(R.drawable.heart1);
+                break;
+            case 1:
+                sajatHp1.setImageResource(R.drawable.heart1);
+                break;
+            default:
+                break;
+        }
+        sajatElet--;
+        /*if (sajatElet < 1){
+            builder.setTitle("Vesztettél").create().show();
+        }*/
+    }
+
+    private void GepEletLevon() {
+
+        switch (gepElet){
+
+            case 3:
+                gepHp3.setImageResource(R.drawable.heart1);
+                break;
+            case 2:
+                gepHp2.setImageResource(R.drawable.heart1);
+                break;
+            case 1:
+                gepHp1.setImageResource(R.drawable.heart1);
+                break;
+            default:
+                break;
+        }
+        gepElet--;
+       /* if (gepElet < 1){
+            builder.setTitle("Vesztettél").create().show();
+        }*/
     }
 
     private void init() {
+        sajatElet=3;
+        gepElet=3;
         gepHp1=findViewById(R.id.gepHp1);
         gepHp2=findViewById(R.id.gepHp2);
         gepHp3=findViewById(R.id.gepHp3);
-        gepValasz=findViewById(R.id.image_gepValasz);
+        imageGepValasz=findViewById(R.id.image_gepValasz);
         sajatHp1=findViewById(R.id.sajatHp1);
         sajatHp2=findViewById(R.id.sajatHp2);
         sajatHp3=findViewById(R.id.sajatHp3);
-        sajatValasz=findViewById(R.id.image_sajatValasz);
+        imageSajatValasz=findViewById(R.id.image_sajatValasz);
         btnRock=findViewById(R.id.btnRock);
         btnPaper=findViewById(R.id.btnPaper);
         btnScissor=findViewById(R.id.btnScissor);
